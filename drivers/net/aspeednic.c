abb97a04241839c4358854ab89f698cb3de4d82c
Joel Stanley <joel@jms.id.au>
2015-11-23 10:42:22 +1030

    Add Aspeed AST2400 palmetto board

--- drivers/net/aspeednic.c
+++ drivers/net/aspeednic.c
@@ -9,6 +9,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <config.h>
 #include <common.h>
 
 #if defined(CONFIG_CMD_NET) && defined(CONFIG_NET_MULTI) && defined(CONFIG_ASPEEDNIC)
