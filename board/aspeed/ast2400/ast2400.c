26dda847f2e082618879d7f51217e988796f7a2b
Joel Stanley <joel@jms.id.au>
2016-04-08 15:37:28 +0930

    aspeed: Clean up board init
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- board/aspeed/ast2400/ast2400.c
+++ board/aspeed/ast2400/ast2400.c
@@ -1,12 +1,13 @@
 /*
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * Aspeed ast2400
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Copyright 2016 IBM Corp.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
  */
 
 #include <common.h>
@@ -14,74 +15,70 @@
 #include <command.h>
 #include <pci.h>
 
+#define SCU_BASE	0x1e6e2000
+#define	GPIO_BASE	0x1e780000
+#define AHB_BASE	0x1e600000
+#define SMC_BASE	0x1e620000
+#define WDT_BASE 	0x1e785000
+
+#define SCU_KEY		0x1688a8a8
+#define AHB_KEY		0xaeed1a03
+
 int board_init(void)
 {
 	DECLARE_GLOBAL_DATA_PTR;
-	unsigned long gpio;
+	unsigned long gpio_I2;
 	unsigned long reg;
 
 	/* AHB Controller */
-	*((volatile ulong*) 0x1E600000)  = 0xAEED1A03;	/* unlock AHB controller */
-	*((volatile ulong*) 0x1E60008C) |= 0x01;	/* map DRAM to 0x00000000 */
+	writel(AHB_KEY, AHB_BASE | 0x00);			/* unlock AHB controller */
+	setbits(le32, AHB_BASE | 0x8c, 0x01);			/* map DRAM to 0x00000000 */
 
 	/* Flash Controller */
-	*((volatile ulong*) 0x1e620000) |= 0x800f0000;	/* enable Flash Write */
+	setbits(le32, SMC_BASE | 0x00, 0x800f0000);		/* enable Flash Write */
 
 	/* SCU */
-	*((volatile ulong*) 0x1e6e2000) = 0x1688A8A8;	/* unlock SCU */
-	reg = *((volatile ulong*) 0x1e6e2008);
-	reg &= 0x1c0fffff;
-	reg |= 0x61800000;				/* PCLK  = HPLL/8 */
+	writel(SCU_KEY, SCU_BASE | 0x00);			/* unlock SCU */
 
-	//check lpc or lpc+ mode
-	gpio = *((volatile ulong*) 0x1e780070);		/* mode check */
-	if(gpio & 0x2)
+	reg = readl(0x1e6e2009);
+	reg &= 0x1c0fffff;
+	reg |= 0x61800000;                              /* PCLK  = HPLL/8 */
+
+	/* Check lpc or lpc+ mode */
+	gpio_I2 = readl(GPIO_BASE | 0x0070) & 0x02;
+	if (gpio_I2)
 		reg |= 0x100000;				/* LHCLK = HPLL/4 */
 	else
 		reg |= 0x300000;				/* LHCLK = HPLL/8 */
 
 	reg |= 0x80000; 				/* enable LPC Host Clock */
 
-	*((volatile ulong*) 0x1e6e2008) = reg;
+	writel(reg, SCU_BASE | 0x08);
 
-	reg = *((volatile ulong*) 0x1e6e200c);		/* enable LPC clock */
-	*((volatile ulong*) 0x1e6e200c) &= ~(1 << 28);
+	reg = readl(SCU_BASE | 0x0c);			/* enable LPC clock */
+	clrbits(le32, SCU_BASE | 0x0c, 1 << 28);
 
-	if(gpio & 0x2) {
-		//use LPC+ for sys clk
-		// set OSCCLK = VPLL1
-		*((volatile ulong*) 0x1e6e2010) = 0x18;
+	if (gpio_I2) {
+		/* use LPC+ for sys clk */
+		/* set OSCCLK = VPLL1 */
+		writel(0x18, SCU_BASE | 0x10);
 
-		// enable OSCCLK
-		reg = *((volatile ulong*) 0x1e6e202c);
-		reg |= 0x00000002;
-		*((volatile ulong*) 0x1e6e202c) = reg;
+		/* Enable OSCCLK */
+		setbits(le32, SCU_BASE | 0x2c, 0x02);
 	} else {
-		// USE LPC use D2 clk
-		/*set VPPL1 */
-		*((volatile ulong*) 0x1e6e201c) = 0x6420;
+		/* Use LPC use D2 clk */
+		/* Set VPPL1 */
+		writel(0x6420, SCU_BASE | 0x1c);
 
-		// set d2-pll & enable d2-pll D[21:20], D[4]
-		reg = *((volatile ulong*) 0x1e6e202c);
-		reg &= 0xffcfffef;
-		reg |= 0x00200010;
-		*((volatile ulong*) 0x1e6e202c) = reg;
+		/* set d2-pll & enable d2-pll D[21:20], D[4] */
+		clrsetbits(le32, SCU_BASE | 0x2c, 0x00300010, 0x00200010);
 
-		// set OSCCLK = VPLL1
-		*((volatile ulong*) 0x1e6e2010) = 0x8;
+		/* set OSCCLK = VPLL1 */
+		writel(0x8, SCU_BASE | 0x10);
 
-		// enable OSCCLK
-		reg = *((volatile ulong*) 0x1e6e202c);
-		reg &= 0xfffffffd;
-		reg |= 0x00000002;
-		*((volatile ulong*) 0x1e6e202c) = reg;
+		/* enable OSCCLK */
+		clrsetbits(le32, SCU_BASE | 0x2c, 0x00300010, 0x02);
 	}
-	reg = *((volatile ulong*) 0x1e6e200c);		/* enable 2D Clk */
-	*((volatile ulong*) 0x1e6e200c) &= 0xFFFFFFFD;
-	/* enable wide screen. If your video driver does not support wide screen, don't
-	   enable this bit 0x1e6e2040 D[0]*/
-	reg = *((volatile ulong*) 0x1e6e2040);
-	*((volatile ulong*) 0x1e6e2040) |= 0x01;
 
 	/* arch number */
 	gd->bd->bi_arch_number = MACH_TYPE_ASPEED;
@@ -105,15 +102,14 @@ int dram_init (void)
 static void watchdog_init(void)
 {
 #ifdef CONFIG_ASPEED_ENABLE_WATCHDOG
-#define AST_WDT_BASE 0x1e785000
 #define AST_WDT_CLK (1*1000*1000) /* 1M clock source */
 	u32 reload = AST_WDT_CLK * CONFIG_ASPEED_WATCHDOG_TIMEOUT;
 	/* set the reload value */
-	__raw_writel(reload, AST_WDT_BASE + 0x04);
+	writel(reload, WDT_BASE | 0x04);
 	/* magic word to reload */
-	__raw_writel(0x4755, AST_WDT_BASE + 0x08);
+	writel(0x4755, WDT_BASE | 0x08);
 	/* start the watchdog with 1M clk src and reset whole chip */
-	__raw_writel(0x33, AST_WDT_BASE + 0x0c);
+	writel(0x33, WDT_BASE | 0x0c);
 	printf("Watchdog: %us\n", CONFIG_ASPEED_WATCHDOG_TIMEOUT);
 #endif
 }
@@ -123,32 +119,28 @@ int misc_init_r(void)
     unsigned int reg1, revision, chip_id;
 
     /* Show H/W Version */
-    reg1 = (unsigned int) (*((ulong*) 0x1e6e207c));
+    reg1 = readl(SCU_BASE | 0x7c);
     chip_id = (reg1 & 0xff000000) >> 24;
     revision = (reg1 & 0xff0000) >> 16;
 
     puts ("H/W:   ");
-    if (chip_id == 1) {
-	if (revision >= 0x80) {
-		printf("AST2300 series FPGA Rev. %02x \n", revision);
-	}
-	else {
-		printf("AST2300 series chip Rev. %02x \n", revision);
-	}
-    }
-    else if (chip_id == 2) {
+    switch (chip_id) {
+    case 2:
 	printf("AST2400 series chip Rev. %02x \n", revision);
-    }
-    else if (chip_id == 0) {
-		printf("AST2050/AST2150 series chip\n");
+	break;
+    case 1:
+	printf("AST2300 series Rev. %02x \n", revision);
+	break;
+    case 0:
+	printf("AST2050/AST2150 series chip\n");
+	break;
     }
 
-    if (getenv ("verify") == NULL) {
-	setenv ("verify", "n");
-    }
-    if (getenv ("eeprom") == NULL) {
-	setenv ("eeprom", "y");
-    }
+    if (getenv("verify") == NULL)
+	setenv("verify", "n");
+
+    if (getenv("eeprom") == NULL)
+	setenv("eeprom", "y");
 
     watchdog_init();
     return 0;
@@ -156,8 +148,5 @@ int misc_init_r(void)
 
 int board_eth_init(bd_t *bis)
 {
-	int ret = -1;
-	ret = aspeednic_initialize(bis);
-
-	return ret;
+	return aspeednic_initialize(bis);
 }
