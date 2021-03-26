e7f68eeca882b26320d196948dbbed05e10bc56f
Joel Stanley <joel@jms.id.au>
2021-03-26 16:15:25 +1030

    aspeed: hace: hacky implementation of sg mode
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- drivers/crypto/aspeed_hace.c
+++ drivers/crypto/aspeed_hace.c
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
+#include <stdlib.h>
+
 #include <common.h>
 #include <log.h>
 #include <asm/io.h>
@@ -88,21 +90,57 @@ void hw_sha512(const unsigned char *pbuf, unsigned int buf_len,
 		debug("HACE failure\n");
 }
 
+struct aspeed_sg {
+	uint32_t addr;
+	uint32_t size;
+};
+
+#define HACE_SG_MAX	128
+
+struct ctx {
+	struct aspeed_sg sg[HACE_SG_MAX];
+	int count;
+};
+
 #if IS_ENABLED(CONFIG_SHA_PROG_HW_ACCEL)
 int hw_sha_init(struct hash_algo *algo, void **ctxp)
 {
-	return -ENOTSUPP;
+	enable_crypto();
+
+	*ctxp = calloc(1, sizeof(struct ctx));
+
+	return 0;
 }
 
 int hw_sha_update(struct hash_algo *algo, void *ctx, const void *buf,
 			    unsigned int size, int is_last)
 {
-	return -ENOTSUPP;
+	struct ctx *c = ctx;
+	struct aspeed_sg *sg;
+
+	if (c->count > HACE_SG_MAX)
+		return -EINVAL;
+
+	sg = &c->sg[c->count++];
+	sg->addr = (uint32_t)buf;
+	sg->size = size;
+
+	if (is_last)
+		sg->addr |= BIT(31);
+
+	return 0;
 }
 
 int hw_sha_finish(struct hash_algo *algo, void *ctx, void *dest_buf,
 		     int size)
 {
-	return -ENOTSUPP;
+	struct ctx *c = ctx;
+
+	writel((u32)c->sg, ASPEED_HACE_HASH_SRC);
+	writel((u32)dest_buf, ASPEED_HACE_HASH_DIGEST_BUFF);
+	writel(c->count * sizeof(struct aspeed_sg), ASPEED_HACE_HASH_DATA_LEN);
+	writel(HACE_SHA_BE_EN | HACE_SG_EN | HACE_ALGO_SHA512, ASPEED_HACE_HASH_CMD);
+
+	return ast_hace_wait_isr(ASPEED_HACE_STS, HACE_HASH_ISR, 1000);
 }
 #endif
